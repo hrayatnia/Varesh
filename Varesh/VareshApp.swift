f0b98890e05ef4f010d3c700f79756bc412ad695
Sam Ray <h.rayatnia@gmail.com>
2022-11-25 13:26:26 +0100

    Initial Commit

---
+++ Varesh/VareshApp.swift
@@ -0,0 +1,17 @@
+//
+//  VareshApp.swift
+//  Varesh
+//
+//  Created by Sam Ray on 2022-11-25.
+//
+
+import SwiftUI
+
+@main
+struct VareshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
