e7d4ca52d7b705a3cc8b600fac0ce55f5030758f
Sam Ray <h.rayatnia@gmail.com>
2022-11-27 23:19:12 +0100

    fixing coordinator build problem

--- Varesh/Presentation/Coordinator/AppCoordinator/AppCoordinator.swift
+++ Varesh/Presentation/Coordinator/AppCoordinator/AppCoordinator.swift
@@ -3,11 +3,11 @@ import SwiftUI
 import Stinsen
 
 final class AppCoordinator: NavigationCoordinatable {
-    var stack: NavigationStack<AppCoordinator> =  NavigationStack(initial: \.start)
-    
+    var stack: Stinsen.NavigationStack<AppCoordinator> =  Stinsen.NavigationStack(initial: \.start)
+
     @Root var start = makeStart
-    
+
     func makeStart() -> some View {
-        return ContentView(viewModel: ContentViewModel())
+        ContentView(viewModel: ContentViewModel())
     }
 }
